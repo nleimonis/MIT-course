8f6728611551125b9b9d656e1163bcf23cac3d47
nleimonis <nleimonis@gmail.com>
2026-04-01 17:57:00 +0300

    added personal welcome

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -6,11 +6,15 @@ with your name. Then, save the code, add it to the staging area, and commit it t
 """
 
 def main():
+    city="Rhodes" # TODO: Insert the city you live in between the double quotes
+    country="Greece" # TODO: Insert the country you live in between the double quotes
     print("Hello World!")
 
     name = "Nikolas" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print(f"I live in {city}, {country}. I like football and I'm 14 years old.")
+
 
 if __name__ == "__main__":
     main()
